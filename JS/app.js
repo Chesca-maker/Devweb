ea02575cefebb851d83d1b8c62177a74c3186c7b
Chesca-maker <eah28chra@csf.bc.ca>
2026-06-11 14:42:24 -0700

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,120 @@
 
+      const form = document.querySelector("form"),
+      nameField1 = form.querySelector(".name-field1"),
+      nameInput1 = emailField1.querySelector(".namefield"),
+      emailField1 = form.querySelector(".email-field1"),
+      emailInput1 = emailField1.querySelector(".email1"),
+      passField1 = form.querySelector(".psw1-field1"),
+      passInput1 = passField1.querySelector(".pass1"),
+      cPassField1 = form.querySelector(".psw2-field1"),
+      cPassInput1 = cPassField1.querySelector(".pass2");
+
+      function nomutilisateur() {
+        let namefield = document.getElementById("namefield").value;
+        let message = document.getElementById("message");
+
+        if(namefield.length != 0){
+            message.classList.remove("invalid");
+            passField1.classList.remove("invalid");
+            message.style.display = 'none';
+          }
+          else {
+            message.classList.add("invalid");
+            passField1.classList.add("invalid");
+            message.style.display = 'block';
+          }
+      }
+
+      function arobase(){
+        const passwordInp = document.querySelector(".email1");
+        const message = document.querySelectorAll(".message1");
+
+        const requirements = [
+          { regex: /[@]/}
+        ]
+
+        passwordInp.addEventListener("keyup", () => {
+          requirements.forEach((item, i) => {
+
+            let isValid = item.regex.test(passwordInp.value);
+
+            if(message[i].length != 0){
+              if(isValid) {
+                message[i].classList.remove("invalid");
+                emailField1.classList.remove("invalid");
+                message[i].style.display = 'none';
+              } else{
+                message[i].classList.add("invalid");
+                emailField1.classList.remove("invalid");
+                message[i].style.display = 'block';
+              }
+            }
+          })
+        })
+      }
+
+      function validate1(){
+        let password = document.getElementById("pass1").value;
+        let cnfrmPassword = document.getElementById("pass2").value;
+        let message = document.getElementById("message2");
+
+        if(password.length != 0){
+          if(password == cnfrmPassword){
+            message.classList.remove("invalid");
+            passField1.classList.remove("invalid");
+            message.style.display = 'none';
+          }
+          else {
+            message.classList.add("invalid");
+            passField1.classList.add("invalid");
+            message.style.display = 'block';
+          }
+        }
+      }
+
+      function validate2(){
+        let password = document.getElementById("pass1").value;
+        let cnfrmPassword = document.getElementById("pass2").value;
+        let message = document.getElementById("message3");
+
+        if(cnfrmPassword.length != 0){
+          if(password == cnfrmPassword){
+            message.classList.remove("invalid");
+            cPassField1.classList.remove("invalid");
+            message.style.display = 'none';
+          }
+          else {
+            message.classList.add("invalid");
+            cPassField1.classList.add("invalid");
+            message.style.display = 'block';
+          }
+        }
+      }
+
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        arobase();
+        validate1();
+        validate2();
+
+        emailInput1.addEventListener("keyup", arobase);
+        passInput1.addEventListener("keyup", validate1);
+        cPassInput1.addEventListener("keyup", validate2);
+
+        if (
+          !emailField1.classList.contains("invalid") &&
+          !passField1.classList.contains("invalid") &&
+          !cPassField1.classList.contains("invalid")
+        ) {
+          location.href = form.getAttribute("action");
+          localStorage.setItem('name', getName.value);
+          localStorage.setItem('email', getEmail.value);
+          localStorage.setItem('password', getPassword.value);
+
+          nameDisplayCheck();
+        }
+      });
+
       const greeting = document.querySelector('.greeting');
       const greeting2 = document.querySelector('.greeting2');
       const rememberMe = document.querySelector('.input-group1');
@@ -25,7 +141,6 @@
           greeting.textContent = `${name}`;
           greeting2.textContent = `Bonjour ${name}! Vous êtes déjà connecté. Oublie-toi?`;
           greeting2.style.color = '#341539';
-          title.textContent = 'Connexion';
           forgetMe.style.display = 'block';
           rememberMe.style.display = 'none';
           opt2.classList.add("opt2");
@@ -43,7 +158,8 @@
           opt15.classList.remove("opt15");
         } else {
           greeting.textContent = '';
-          title.textContent = 'Inscription';
+          greeting2.textContent = 'Bienvenue!';
+          greeting2.style.display = 'none';
           forgetMe.style.display = 'none';
           rememberMe.style.display = 'block';
           opt2.classList.add("opt2");
@@ -60,3 +176,6 @@
           opt15.classList.add("opt15");
         }
       }
+
+      document.body.onload = nameDisplayCheck;
+      console.log(localStorage.getItem('name'));
